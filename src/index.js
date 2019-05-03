b61bf19e528f8a495f35600addb3ea5301dbe584
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-05-03 23:57:08 +0530

    started migrating the code to React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+const test = document.querySelector(".test");
+
+class TwitterCard extends React.Component {
+  render() {
+    return (
+      <div className="tweet-card">
+        <div className="user-info">
+          <div className="avatar">
+            <img
+              className="profile-pic"
+              src="http://bit.do/eQzFo"
+              alt="profile-photo"
+            />
+            <div className="user-name">
+              <div className="name">Random user</div>
+              <div className="handle">@randomUser</div>
+            </div>
+          </div>
+          <div className="options">
+            <button className="follow">Follow</button>
+            <div className="more-options">...</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TwitterCard />, test);
