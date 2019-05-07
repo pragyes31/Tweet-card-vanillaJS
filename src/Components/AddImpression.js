d7f7e4280d63d5f87e80a8b391d56a6865a246e5
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-05-07 17:02:28 +0530

    addImpression component updated

--- src/Components/AddImpression.js
+++ src/Components/AddImpression.js
@@ -1,12 +1,24 @@
 import React from "react";
+import commentBtn from "../images/comment_twitter.png";
+import retweetBtn from "../images/retweet.png";
+import likeBtn from "../images/like-twitter.png";
 
 export default class AddImpression extends React.Component {
   render() {
     return (
       <div className="add-impressions">
-        <div className="comment" />
-        <div className="retweet" />
-        <div className="like" />
+        <div className="comment">
+          <img className="comment-img" src={commentBtn} alt="" />
+          <div className="comment-count">1.2k</div>
+        </div>
+        <div className="retweet">
+          <img className="retweet-img" src={retweetBtn} alt="" />
+          <div className="retweet-count">1.2k</div>
+        </div>
+        <div className="like">
+          <img className="like-img" src={likeBtn} alt="" />
+          <div className="like-count">1.2k</div>
+        </div>
       </div>
     );
   }
